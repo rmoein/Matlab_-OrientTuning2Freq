c5718c7fc3053fc9dce2808c0703871327944786
Reza <rezamoeint@gmail.com>
2018-03-08 15:32:31 -0800

    new files for the project

---
+++ readPowerDiva.m
@@ -0,0 +1,10 @@
+clear; clc;
+
+cd('/Users/reza/Documents/Matlab_-OrientTuning2Freq');
+
+addpath(genpath('/Users/reza/Dropbox/Research_Stuff/Neuro/Justin Gardner/Exp_MATL_HCN_128_Avg(Full data)'));
+
+addpath(genpath('C:\Users\rezam\Dropbox\Research_Stuff\Neuro\Justin Gardner'));
+
+
+
