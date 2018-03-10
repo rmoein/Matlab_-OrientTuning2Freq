fea3d2d2b078c497acb362d0ce81d57c872444a4
Reza <rezamoeint@gmail.com>
2018-03-09 17:51:16 -0800

    working on the readPowerDiva fucntion

--- fileNameChecker.m
+++ fileNameChecker.m
@@ -1,12 +1,13 @@
 function z = fileNameChecker(file_name,target_word)
 % the point of this function is to break apart the name of the files and
 % return true if the name contains the target_word
-target_word = lower(target_word);
+% target_word = lower(target_word);
 
 file_name = strsplit(file_name,{'.','_'});
 
 for i = 1: length(file_name)
-    if strcmp(lower(char(file_name(i))), target_word) == 1
+%     disp(char(file_name(i)))
+    if strcmp(char(file_name(i)), target_word) == 1
         z = 1;
         return 
     else
--- readPowerDiva.m
+++ readPowerDiva.m
@@ -1,4 +1,4 @@
-function z = readPowerDiva(read_raw)
+function ch75 = readPowerDiva(read_raw)
 
     cd('/Users/reza/Documents/Matlab_-OrientTuning2Freq');
 
@@ -7,6 +7,10 @@ function z = readPowerDiva(read_raw)
     addpath(genpath('C:\Users\rezam\Dropbox\Research_Stuff\Neuro\Justin Gardner'));
 
     addpath(genpath('/Users/reza/mrTools'));
+    
+    addpath(genpath('/Users/reza/Documents/Matlab_-OrientTuning2Freq'));
+    
+    cd('/Users/reza/Dropbox/Research_Stuff/Neuro/Justin Gardner/Exp_MATL_HCN_128_Avg(Full data)');
 
 % read_raw:
 % - 0 for raw EEG
@@ -17,9 +21,44 @@ file_directory = uigetdir();
 files = dir(fullfile(file_directory, '*.mat'));
 
 if read_raw == 0
-    for i = 1:length(files)
-        fileNameChecker(files(i), 'Raw')
-        z = disp(files(i))
+    for c = 1:length(files) % this loop goes over all the files
+        
+            t = 1;
+            B = [];% this needs to be blank before each condition loop because it will contain the data for each condition
+        
+        if fileNameChecker(files(c).name, 'Raw') == 1 % this checks to see if the name contains 'raw'
+%             disp(files(i).name)
+            % this if statement checks to see if the current file and
+            % previous file have the same number of condition
+            if c == 1
+                compare = [];
+                cond_num  = strsplit(files(c).name, '_');
+                cond_num = cond_num(2);
+            else
+                cond_num  = strsplit(files(c).name, '_');
+                compare = strsplit(files(c-1).name, '_');
+            end
+            
+            if c <10
+                c = strcat('0',num2str(c));
+            else 
+                c = num2str(c);
+            end
+            % this if statement separates the data from each condition
+            if cond_num == compare
+                
+                temp = load(strcat('Raw_c0', num2str(c),'_t0', num2str('01'),'.mat'));
+                raw_data = reshape(temp.RawTrial,[size(temp.RawTrial,1)/temp.NmbEpochs, temp.NmbEpochs , 130]);
+                B = cat(4, B, raw_data);
+            else
+                temp = load(strcat('Raw_c0', num2str(c),'_t0', num2str('01'),'.mat'));
+                raw_data = reshape(temp.RawTrial,[size(temp.RawTrial,1)/temp.NmbEpochs, temp.NmbEpochs , 130]);
+                
+            try
+                
+                
+            end
+        end
     end
 end
 
